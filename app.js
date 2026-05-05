87c54af4b1594afdbccc5329cb27359db4b29585
Yasas Raj <yasasraj@gmail.com>
2026-05-05 13:30:20 -0700

    started working

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'public', 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "public", "views"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  //res.send("Hello World!");
+  res.render("temp");
 });
 
-app.get('/map', (req, res) => {
-  res.render('map');
+app.get("/map", (req, res) => {
+  res.render("map");
 });
 
 app.listen(PORT, () => {
